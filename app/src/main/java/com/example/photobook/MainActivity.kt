c5ede6954d56e5b86fdb4c829a4b44b5b43612b2
root <you@example.com>
2022-05-18 12:02:54 +0200

    sets up the reccuring work

--- app/src/main/java/com/example/photobook/MainActivity.kt
+++ app/src/main/java/com/example/photobook/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.photobook
 
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.*
+import com.example.photobook.work.RefreshDataWork
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 /**
  * MainActivity - Class that use our navigation
@@ -15,6 +22,40 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
+        delayedInit()
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * delayedInit - Starts a coroutine scope to start work
+     */
+    private fun delayedInit() {
+        CoroutineScope(Dispatchers.Default).launch {
+            setUpRecurringWork()
+        }
+    }
+
+    /**
+     * setUpRecurringWork - Sets up a recurring work,
+     * retrieving data from firestore and saving them in local db
+     */
+    private fun setUpRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }.build()
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWork>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWork.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest)
     }
 }
--- app/src/main/java/com/example/photobook/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/photobook/work/RefreshDataWork.kt
@@ -3,14 +3,31 @@ package com.example.photobook.work
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.example.photobook.repository.Repository
+import com.example.photobook.repository.database.PhotoBookDatabase
+import com.example.photobook.repository.network.RemoteRepository
 
 class RefreshDataWork(
     appContext: Context,
     params: WorkerParameters
 ): CoroutineWorker(appContext, params)
 {
+    companion object{
+        const val WORK_NAME = "RefreshDataWorker"
+    }
+
     override suspend fun doWork(): Result
     {
-        TODO("Not yet implemented")
+        val db = PhotoBookDatabase.getInstance(applicationContext)
+        val repository = Repository(db.photoBookDao, RemoteRepository())
+        return try
+        {
+            repository.refreshPosts()
+            Result.success()
+        }
+        catch (e:Exception)
+        {
+        Result.retry()
+        }
     }
 }
