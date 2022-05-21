efe1811c639e93e79709aa967d32c8026c9332c8
root <you@example.com>
2022-05-21 22:35:48 +0100

    displays image in detail fragment

--- app/src/main/java/com/example/photobook/detail/DetailFragment.kt
+++ app/src/main/java/com/example/photobook/detail/DetailFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.photobook.data.PostFirestore
 import com.example.photobook.databinding.FragmentDetailBinding
+import com.example.photobook.main.MainViewModel
 import com.google.android.material.snackbar.Snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -22,6 +23,7 @@ class DetailFragment : Fragment()
     private lateinit var binding: FragmentDetailBinding
     private lateinit var selectedPost: PostFirestore
     private val _viewModel: DetailViewModel by viewModel()
+    private val _mainViewModel: MainViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,6 +46,24 @@ class DetailFragment : Fragment()
                     Snackbar.LENGTH_SHORT
                 ).show()
                 _viewModel.onSnackBarShowed()
+            }
+        }
+        if (selectedPost.media != null)
+        {
+            for (i in 0 until selectedPost.media!!.url.size)
+            {
+                _mainViewModel.getImage(selectedPost.media!!.url[i])
+                _mainViewModel.image.observe(viewLifecycleOwner) {
+                    image ->
+                    when (i)
+                    {
+                        0 -> binding.imageVal0 = image
+                        1 -> binding.imageVal1 = image
+                        2 -> binding.imageVal2 = image
+                        3 -> binding.imageVal3 = image
+                        4 -> binding.imageVal4 = image
+                    }
+                }
             }
         }
 
